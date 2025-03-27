2fd9e9402fee65b56c77150f4e991e83b5b649d5
MichaelLarryOchieng <ochiengmichaellarry@gmail.com>
2025-03-28 00:01:01 +0300

    Update settings.py

--- locations/location_proj/settings.py
+++ locations/location_proj/settings.py
@@ -14,5 +14,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-d1kcnem21+r64*g4oku-09-(thsm67di)^ire-#23*8$wya5k+'
 
 
