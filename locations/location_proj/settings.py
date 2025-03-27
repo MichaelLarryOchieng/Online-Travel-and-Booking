461a37742f8bc0ab4e275fbe763184b0fc7efe3d
MichaelLarryOchieng <ochiengmichaellarry@gmail.com>
2025-03-28 00:00:33 +0300

    Update settings.py

--- locations/location_proj/settings.py
+++ locations/location_proj/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
