1bd68ed8e50da796fe1bebef4db12836fa65e2ae
MichaelLarryOchieng <ochiengmichaellarry@gmail.com>
2025-03-29 23:08:22 +0300

    Update settings.py

--- locations/location_proj/settings.py
+++ locations/location_proj/settings.py
@@ -40,4 +40,5 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
 
