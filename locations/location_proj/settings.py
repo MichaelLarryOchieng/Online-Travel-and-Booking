94972fe820d3dbf0afd04618c3cb4bfbb878156c
MichaelLarryOchieng <ochiengmichaellarry@gmail.com>
2025-04-03 23:26:17 +0300

    Update settings.py

--- locations/location_proj/settings.py
+++ locations/location_proj/settings.py
@@ -53,4 +53,8 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
 
