8c432c0a8c09208448e96e6b3b36a9507c60454e
MichaelLarryOchieng <ochiengmichaellarry@gmail.com>
2025-04-09 23:59:38 +0300

    Update settings.py

--- locations/location_proj/settings.py
+++ locations/location_proj/settings.py
@@ -83,6 +83,11 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+
 
 
 
