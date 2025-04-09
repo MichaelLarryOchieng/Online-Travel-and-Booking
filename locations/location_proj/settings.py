db1794abc06462f99d0c76f710995e7021f00957
MichaelLarryOchieng <ochiengmichaellarry@gmail.com>
2025-04-10 00:00:08 +0300

    Update settings.py

--- locations/location_proj/settings.py
+++ locations/location_proj/settings.py
@@ -87,6 +87,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
 
 
 
