e61df8c8c53805001a52aa2e1e65b6d01df29351
MichaelLarryOchieng <ochiengmichaellarry@gmail.com>
2025-04-10 00:01:54 +0300

    Update settings.py

--- locations/location_proj/settings.py
+++ locations/location_proj/settings.py
@@ -93,6 +93,10 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
+     {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
 
 
 
